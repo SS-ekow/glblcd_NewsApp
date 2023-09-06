c4f588bbe5ad697091e4d53dcc9805d444c8bab5
Saani Mustapha Deishini <deishini2003@gmail.com>
2023-09-06 15:10:45 +0000

    api key

--- apiRequests.py
+++ apiRequests.py
@@ -1,39 +1,53 @@
 import requests
-import os
-from dotenv import load_dotenv
-from pprint import pprint
+# from pprint import pprint
 
-load_dotenv()
-apiKey = os.environ.get("apiKey")
+from newsapi import NewsApiClient
+api_key= 'e03943ee0ea8443f95e6e4a671c85ccc'
+newsapi =  NewsApiClient(api_key)
 
-
-if apiKey is None:
-    raise Exception("API key not found. Make sure to set it in your .env file.")
+# if newsapi is None:
+#     raise Exception("API key not found. Make sure to set it in your .env file.")
 
 
-base_url = "https://newsapi.org/v2/top-headlines"
-params = {"country": "ghana",
-          "category": "business", 
-          "apiKey": apiKey}
-
-response = requests.get(base_url, params=params)
-
-if response.status_code == 200:
-    data = response.json()
-    articles = data.get("articles")
+top_headlines = newsapi.get_top_headlines(q = "",
+                                          category = "general",
+                                          language='en',
+                                          country = "au")
 
 
-    for article in articles:
-        print(f"Title: {article['title']}")
-        print(f"Description: {article['description']}")
-        print(f"Source: {article['source']['name']}")
-        print(f"Published At: {article['publishedAt']}")
-        print(f"URL: {article['url']}")
-        print("\n")
+# all_articles = newsapi.get_everything(q='bitcoin',
+#                                     #   sources='bbc-news,the-verge',
+#                                       domains='bbc.co.uk,techcrunch.com',
+#                                       from_param='2023-08-06',
+#                                       to='2023-09-05',
+#                                       language='en',
+#                                       sort_by='relevancy',
+#                                       page=2)
+
+
+# base_url = "https://newsapi.org/v2/top-headlines"
+# params = {"country": "ghana",
+#           "category": "business", 
+#           "apiKey": api_key}
+
+# response = requests.get(base_url, params=params)
+
+# if response.status_code == 200:
+#     data = response.json()
+#     articles = data.get("articles")
+
+
+for article in top_headlines['articles']:
+    print(f"Title: {article['title']}")
+    print(f"Description: {article['description']}")
+    print(f"Source: {article['source']['name']}")
+    print(f"Published At: {article['publishedAt']}")
+    print(f"URL: {article['url']}")
+    print("\n")
     
     
 
-else:
-    print(f"Error: {response.status_code} - {response.text}")
+# else:
+#     print(f"Error: {response.status_code} - {response.text}")
 
 
