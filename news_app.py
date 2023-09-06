0be885302c1bddc2a44cf04c9c154f8ba06cc2d0
Ekow <ekowsackey22@gmail.com>
2023-09-06 14:36:02 +0000

    implemented category queries

--- news_app.py
+++ news_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from newsapi import NewsApiClient
 
-
+newsapi = NewsApiClient(api_key='9a7b975fbe1a468a93b12f0d6b9fce57')
 
 
 app = Flask(__name__)
@@ -14,8 +14,18 @@ def home():
 def sign_up():
     return render_template("signup.html")
 
-@app.route("/category")
-def category():
+@app.route("/category/<cat>")
+def category(cat):
+    
+    category_list = ['business', 'entertainment', 'health', 'science', 'sports', 'technology']
+    if cat in category_list:
+        top_headlines = newsapi.get_top_headlines(category=cat, language='en', country='us')
+        return render_template("category.html", top_headlines=top_headlines)
+    
+    
+    
+    
+    
     return render_template("category.html")
 
 if __name__ == "__main__":
