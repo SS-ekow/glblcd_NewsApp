994733fc246d5f6cf28013492756081048289798
Ekow <ekowsackey22@gmail.com>
2023-09-07 21:08:41 +0000

    made minor changes to UI

--- secret_keys.py
+++ secret_keys.py
@@ -1,3 +1,4 @@
 secret_key ='5791628bb0b13ce0c676dfde280ba245'
 
-apiKey = '9a7b975fbe1a468a93b12f0d6b9fce57'
+# apiKey = '9a7b975fbe1a468a93b12f0d6b9fce57'
+apiKey = '9cbe5dcec05e40019e69ecf418cf713b'
